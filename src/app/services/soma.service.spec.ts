71664e1a3cdec9e51fbc6d46f02a6c5e2e768bb6
Ana Camila <acamilass@gmail.com>
2019-07-04 19:11:09 -0300

    testes http

--- src/app/services/soma.service.spec.ts
+++ src/app/services/soma.service.spec.ts
@@ -1,9 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SomaService } from './soma.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('SomaService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpMock: HttpTestingController = null;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule
+    ]
+  }));
+
+  beforeEach(() => {
+    httpMock = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
     const service: SomaService = TestBed.get(SomaService);
@@ -14,4 +25,23 @@ describe('SomaService', () => {
     const service: SomaService = TestBed.get(SomaService);
     expect(service.soma(2, 5)).toBe(7);
   })
+
+  it('deve receber todos', () => {
+    const service = TestBed.get(SomaService);
+    const fakeData = [
+      { title: 'Titulo 1', description: 'Descrição 1' },
+      { title: 'Titulo 2', description: 'Descrição 2' }
+    ];
+
+    service.getTodos().subscribe(res => {
+      console.log(res);
+      expect(res.length).toBe(2);
+      expect(res[0].date).toBeUndefined();
+      expect(res[1].title).toBe('Titulo 2');
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000');
+    expect(req.request.method).toBe('GET');
+    req.flush(fakeData);
+  });
 });
--- src/app/services/soma.service.ts
+++ src/app/services/soma.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SomaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   soma(n1, n2) {
     return n1 + n2;
+  }
+
+  getTodos() {
+    return this.http.get('http://localhost:3000');
   }
 }
